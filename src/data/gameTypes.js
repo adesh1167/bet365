93844a14800f6c89e8146bc56c1d7a021ec8622c
adesh1167 <54195460+adesh1167@users.noreply.github.com>
2025-06-25 09:20:04 +0100

    Jun 25

--- src/data/gameTypes.js
+++ src/data/gameTypes.js
@@ -1,60 +1,64 @@
 const gameTypes = {
-    "Match Result (1X2)" : {
+    "Match Result (1X2)": {
         text: "Full Time Result",
         callBack: (value) => value
     },
-    "Correct Score" : {
+    "Correct Score": {
         text: "Correct Score",
         callBack: (value, home, away) => {
-            if(value.toLowerCase() === "others"){
-                return "Others";
+            if (value.toLowerCase() === "other") {
+                return "Other";
             }
             const scores = value.split(":").map(Number);
-            if(scores[0] > scores[1]){
-                return `${home} ${scores[0]}-${scores[1]}`
-            } else if(scores[1] > scores[0]){
-                return `${away} ${scores[1]}-${scores[0]}`
+            if (scores.length < 2) {
+                return value;
+            } else {
+                if (scores[0] > scores[1]) {
+                    return `${home} ${scores[0]}-${scores[1]}`
+                } else if (scores[1] > scores[0]) {
+                    return `${away} ${scores[1]}-${scores[0]}`
+                }
+                return `Draw ${scores[0]}-${scores[1]}`
             }
-            return `Draw ${scores[0]}-${scores[1]}`
         }
     },
-    "Both Teams to Score (GG/NG)" : {
+    "Both Teams to Score (GG/NG)": {
         text: "Both Teams To Score",
         callBack: (value) => value === "Yes" ? "Both Teams To Score" : "No"
     },
-    "Double Chance" : {
+    "Double Chance": {
         text: "Double Chance",
         callBack: (value) => value
     },
-    "Overs/Unders" : {
+    "Overs/Unders": {
         text: "Total Goals",
         callBack: (value) => value
     },
-    "1st Half - 1X2" : {
+    "1st Half - 1X2": {
         text: "Half Time Result",
         callBack: (value) => value
     },
-    "Halftime/Fulltime" : {
+    "Halftime/Fulltime": {
         text: "Half Time/Full Time",
         callBack: (value) => value.split("/").join(" - ")
     },
-    "Exact Goals" : {
+    "Exact Goals": {
         text: "Exact Goals",
         callBack: (value) => value
     },
-    "Winning Margin" : {
+    "Winning Margin": {
         text: "Winning Margin",
         callBack: (value) => value.split(" by ").join(" to win by ")
     },
-    "1X2 & Both Teams To Score" : {
+    "1X2 & Both Teams To Score": {
         text: "Result/Both Teams To Score",
         callBack: (value) => value
     },
-    "1X2 and Overs/Unders" : {
+    "1X2 and Overs/Unders": {
         text: "Result/Total Goals",
         callBack: (value) => value
     },
-    "aaa" : {
+    "aaa": {
         text: "bbb",
         callBack: (value) => value
     },
