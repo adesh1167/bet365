2458a9b3736c912943288ccad6b5181aec86f572
adesh1167 <54195460+adesh1167@users.noreply.github.com>
2025-08-18 23:55:49 +0100

    Update url.js

--- src/data/url.js
+++ src/data/url.js
@@ -1,4 +1,4 @@
-export const baseApiUrl = 'https://darkgray-caribou-708880.hostingersite.com/bts-365/handlers';
+export const baseApiUrl = 'https://cywavesinvestment.com/bts-365/handlers';
 export const baseURL = window.location.origin;
 // export const baseApiUrl = 'http://localhost/bts/handlers';
 // export const baseApiUrl = 'http://192.168.14.148/betway/bts/handlers';
